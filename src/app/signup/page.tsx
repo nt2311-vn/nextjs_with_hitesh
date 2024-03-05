e922413b1fae5a51b05b789dbfa7f2ad326f8c68
ntEOS <nguyentoan231196@gmail.com>
2024-03-06 00:08:32 +0700

    add input form and layout for sigun page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,31 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+
 export default function SignUp() {
+	const [user, setUser] = React.useState({
+		email: "",
+		password: "",
+		username: "",
+	});
+
+	const onSignUp = async () => {};
+
 	return (
-		<div className="flex">
-			<h1 className="text-center text-2xl text-white">Sign Up</h1>
+		<div className="flex flex-col items-center justify-center min-h-screen py-2">
+			<h1>Signup</h1>
+			<hr />
+			<label htmlFor="username">username</label>
+			<input
+				className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-300"
+				id="username"
+				type="text"
+				value={user.username}
+				onChange={(e) => setUser({ ...user, username: e.target.value })}
+				placeholder="username"
+			/>
 		</div>
 	);
 }
